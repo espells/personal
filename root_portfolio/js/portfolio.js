cb154b215b212a09a316729da015a1937f660fb1
espells <emma.spellacy@gmail.com>
2018-03-17 14:08:56 -0400

    fade in working on about page

---
+++ root_portfolio/js/portfolio.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  //  slow fade in of text, each line individually, with same class.
+
+    $(".emma").delay(250).fadeIn(1500);
+    $(".hithere").delay(1500).fadeIn(2000);
+    $(".context").delay(3000).fadeIn(2000);
+
+
+
+
+// $(document).ready(function(){
+//     $(".hithere").blast({
+//       delimiter: "character",
+//       customClass: "questionChar",
+//       generateIndexID: true
+//     });
+//
+//
+//
+//     $(".blast").hide();
+//
+//     $('.hithere').show();
+//
+//
+//     var currentQuestionChar = 0;
+//
+//     setTimeout(function(){
+//
+//         setInterval(function(){
+//           $("#questionChar-" + currentQuestionChar).show();
+//           currentQuestionChar++;
+//         },60)
+//
+//     },300);
+//
+// });
+
+  });
